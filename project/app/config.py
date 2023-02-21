6006b16c0ceab36cd364913928020eef0391369b
lubomir.angelov <lubomir.angelov.an@gmail.com>
2023-02-21 20:56:56 +0200

    finish live demo

--- project/app/config.py
+++ project/app/config.py
@@ -4,7 +4,7 @@
 import logging
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 
 log = logging.getLogger("uvicorn")
@@ -13,6 +13,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = 0
+    database_url: AnyUrl = None
 
 
 @lru_cache()
