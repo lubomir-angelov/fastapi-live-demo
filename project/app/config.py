eed5990dec22c37e10a5e22822b399587d46fb51
lubomir.angelov <lubomir.angelov.an@gmail.com>
2023-02-21 19:29:42 +0200

    init app live demo

---
+++ project/app/config.py
@@ -0,0 +1,21 @@
+# project/app/config.py
+
+
+import logging
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = 0
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
